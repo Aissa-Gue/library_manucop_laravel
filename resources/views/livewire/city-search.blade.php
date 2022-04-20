d8ec813ffa763251aea74f64492924a9fb14fc35
Aissa-Gue <AissaStarDz@gmail.com>
2022-04-20 08:03:32 +0100

    fix style

--- resources/views/livewire/city-search.blade.php
+++ resources/views/livewire/city-search.blade.php
@@ -1,18 +1,11 @@
 <div>
-    <label for="city" class="form-label">{{$cityLivewire['label']}}</label>
-    <input type='text'
-           placeholder='{{$cityLivewire['placeholder']}}'
-           class='form-control'
-           list='cities'
-           wire:model="city"
-           id="city"
-           name="city_name"
-           onchange="getId('#city','#cities','city_id_hidden')">
+    <label for="city" class="form-label">{{ $cityLivewire['label'] }}</label>
+    <input type='text' placeholder='{{ $cityLivewire['placeholder'] }}' class='form-select' list='cities'
+        wire:model="city" id="city" name="city_name" onchange="getId('#city','#cities','city_id_hidden')">
 
     <datalist id="cities">
         @foreach ($cities as $city)
-            <option value="{{$city->name}}" data-id="{{$city->id}}">
+            <option value="{{ $city->name }}" data-id="{{ $city->id }}">
         @endforeach
     </datalist>
 </div>
-
--- resources/views/livewire/country-search.blade.php
+++ resources/views/livewire/country-search.blade.php
@@ -1,17 +1,12 @@
 <div>
-    <label for="country" class="form-label">{{$countryLivewire['label']}}</label>
-    <input type='text'
-           class="form-select"
-           placeholder="{{$countryLivewire['placeholder']}}"
-           id="country"
-           list="countries"
-           wire:model="country"
-           name="country_name"
-           onchange="getId('#country','#countries','country_id_hidden')">
+    <label for="country" class="form-label">{{ $countryLivewire['label'] }}</label>
+    <input type='text' class="form-select" placeholder="{{ $countryLivewire['placeholder'] }}" id="country"
+        list="countries" wire:model="country" name="country_name"
+        onchange="getId('#country','#countries','country_id_hidden')">
 
     <datalist id="countries">
         @foreach ($countries as $country)
-            <option value="{{$country->name}}" data-id="{{$country->id}}">
+            <option value="{{ $country->name }}" data-id="{{ $country->id }}">
         @endforeach
     </datalist>
 
